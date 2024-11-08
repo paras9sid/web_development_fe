12a9eaa5b2dac2cc2dae00f4565d19afcbe7ee7f
Sidharth Jain <sidharthjainp9@gmail.com>
2024-11-08 17:33:24 +0530

    JS- comments,naming conv,variables-data types intro

--- js/index.js
+++ js/index.js
@@ -1,1 +1,39 @@
-alert("Hello world, external js index file")
+// printin on browser console
+
+// alert("Hello world, external js index file");
+// console.log("print from vscode");
+
+// single line comment
+
+/* 
+multi line comment
+*/
+
+let age = 25;
+console.log(age);
+
+console.log(age > 10);
+
+let myAge = 45;
+console.log(age);
+
+const score = 89;
+console.log(score);
+
+// value of const cant be changed - error 
+// score = 100;
+// console.log(score);
+
+const myScore = 100;
+
+
+/* Data types
+
+number = 10,20,0.05,etc
+string- char or series of char
+boolean= true or false
+
+*/
+
+let myName = "Sidharth jain";
+console.log(myName);

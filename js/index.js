76adcb51a811bd6fb9a9679c1571d1373df1aabe
Sidharth Jain <sidharthjainp9@gmail.com>
2024-11-17 12:15:20 +0530

    JS-Scope-global/local

--- js/index.js
+++ js/index.js
@@ -1,23 +1,32 @@
-// switch statements
+// gloab / local scope
 
-let rating = 13;
 
-switch(rating){
-    case 5:
-        console.log("Must watch!");
-        break;
-    case 4:
-        console.log("good opt!");
-        break;
-    case 3:
-        console.log("Maybe");
-        break;
-    case 2:
-        console.log("Skip!");
-        break;
-    case 1:
-        console.log("Ignore!");
-        break;
-    default:
-        console.log("Invalid!");
+// let score = 70; //global
+
+// if(score < 80){
+//     console.log("less than 80!",score);
+//     if(score > 65){
+//         console.log("Inside 65 block!",score);
+//     }
+// }
+
+// console.log("Inside global block!",score);
+
+//
+
+let score = 70; //global
+
+if(score <= 80){
+    // originally edited global variable
+    score= 90; // value of global changed for everything from now on , in to out everywhere
+    let score2 = 60; // local
+    console.log("less than 80!",score, score2);
+
+    if(score >= 65){
+        score=59; // changed again from 90 to 59
+        console.log("Inside 65 block!",score, score2);
+    }
 }
+
+// console.log("Inside global block!",score, score2); // score 2 error as its local scope not global
+console.log("Inside global block!",score);

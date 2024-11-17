9fec6d76a410f156b042747f5fbd88b164a6635a
Sidharth Jain <sidharthjainp9@gmail.com>
2024-11-17 12:19:06 +0530

    JS-ternary operator

--- js/index.js
+++ js/index.js
@@ -1,32 +1,12 @@
-// gloab / local scope
+// ternary operator
+
+const user = true;
+
+/* ternary op syntax - with expressions
+ condition ? true:false */
+
+user ? console.log("true value") :  console.log("false value!");
 
 
-// let score = 70; //global
-
-// if(score < 80){
-//     console.log("less than 80!",score);
-//     if(score > 65){
-//         console.log("Inside 65 block!",score);
-//     }
-// }
-
-// console.log("Inside global block!",score);
-
-//
-
-let score = 70; //global
-
-if(score <= 80){
-    // originally edited global variable
-    score= 90; // value of global changed for everything from now on , in to out everywhere
-    let score2 = 60; // local
-    console.log("less than 80!",score, score2);
-
-    if(score >= 65){
-        score=59; // changed again from 90 to 59
-        console.log("Inside 65 block!",score, score2);
-    }
-}
-
-// console.log("Inside global block!",score, score2); // score 2 error as its local scope not global
-console.log("Inside global block!",score);
+const username = "sidharth";
+username.length >= 5 ? console.log("above 5 ") : console.log("less tahn 5");

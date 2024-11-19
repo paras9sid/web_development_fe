f4bdd5948082297495b9d7231e529efd71c24de7
Sidharth Jain <sidharthjainp9@gmail.com>
2024-11-19 20:01:53 +0530

    JS-foreach method

--- js/index.js
+++ js/index.js
@@ -1,39 +1,26 @@
-// callback fucntions
+// foreach method & callback
 
-let addition = function(a,b){
-    return a + b;
-}
+// let products = ["bread", "butter", "egg", "bottle"];
 
-let subtract = function(a,b){
-    return a - b;
-}
+//syntax = products.forEach();
 
-let multiply = function(a,b){
-    return a * b;
-}
+// fucntion for callback
 
-let divide = function(a,b){
-    return a / b;
-}
+// let callback = (product) => {console.log(`My product name is: ${product}`)};
 
-// function name args can be any - addition - add , mutiply - mul
-function calculator(numOne, numTwo, add, sub, mul, div){
-    // let addResult = add(numOne, numTwo);
-    // let mulResult = mul(numOne, numTwo);
-    
-    // with console.log
-    // console.log(`Add: ${addResult}, Multiply: ${mulResult}`);
+// products.forEach(callback);
 
-    //with return
-    // return `Add: ${addResult}, Multiply: ${mulResult}`;
 
-    // short
-    return `Add: ${add(numOne, numTwo)}, Subtraction: ${sub(numOne, numTwo)}, Multiply: ${mul(numOne, numTwo)}, Division: ${div(numOne, numTwo)}`;
-};
 
-//passing function as an args - wtih console.log in fucntion
-// calculator(5,10, addition, multiply); // name of the original function to be mentioned here
+// let products = ["bread", "butter", "egg", "bottle"];
 
-// with return in function
-console.log(calculator(5,10, addition, subtract, multiply, divide));
+//passing directly 
+// products.forEach( product => console.log(`My product name is : ${product}`) ); // (args brack {} consol.elog brackets removed as one line expressions/code only)
 
+// products.forEach(product=>console.log(product.length));
+
+let students =  ["sidharth","aasha","aliya","nisha"];
+
+students.forEach((student, index)=>{
+    console.log(student, index);
+});

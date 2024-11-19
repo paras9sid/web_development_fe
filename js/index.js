d104872522d4dea122df44aeb894a52897c80dda
Sidharth Jain <sidharthjainp9@gmail.com>
2024-11-19 19:50:53 +0530

    JS-Callback fucntions

--- js/index.js
+++ js/index.js
@@ -1,35 +1,39 @@
-// arrow fundtions in js
+// callback fucntions
 
-let welcome = function(){
-    console.log("WElcome!")
+let addition = function(a,b){
+    return a + b;
+}
+
+let subtract = function(a,b){
+    return a - b;
+}
+
+let multiply = function(a,b){
+    return a * b;
+}
+
+let divide = function(a,b){
+    return a / b;
+}
+
+// function name args can be any - addition - add , mutiply - mul
+function calculator(numOne, numTwo, add, sub, mul, div){
+    // let addResult = add(numOne, numTwo);
+    // let mulResult = mul(numOne, numTwo);
+    
+    // with console.log
+    // console.log(`Add: ${addResult}, Multiply: ${mulResult}`);
+
+    //with return
+    // return `Add: ${addResult}, Multiply: ${mulResult}`;
+
+    // short
+    return `Add: ${add(numOne, numTwo)}, Subtraction: ${sub(numOne, numTwo)}, Multiply: ${mul(numOne, numTwo)}, Division: ${div(numOne, numTwo)}`;
 };
 
-welcome();
+//passing function as an args - wtih console.log in fucntion
+// calculator(5,10, addition, multiply); // name of the original function to be mentioned here
 
-// coversion into arrow function 
+// with return in function
+console.log(calculator(5,10, addition, subtract, multiply, divide));
 
-let intro = () =>{
-    console.log("Arrrow")
-};
-
-intro();
-
-// with args
-
-let intro2 = (name)=>{
-    console.log(`Weclome ${name}`)
-};
-
-intro2("Sidharth");
-
-// with return keyword and decrease the size of funtion
-
-let add = (numOne, numTwo) => numOne + numTwo; // in case of one line of code we can remove {} and term return
-
-console.log(add(2,5));
-
-let fullName = name => `Welcome ${name}`; // no need of () for args if one 1 args
-console.log(fullName("Sidharth Jain")); 
-
-let wel = () => `Welcome`; // empty args
-console.log(wel());

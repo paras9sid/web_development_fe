681e5a493db2b4b30adecfd48c5defa78ccf6ace
Sidharth Jain <sidharthjainp9@gmail.com>
2024-11-20 14:18:45 +0530

    JS-Objects in arrays

--- js/index.js
+++ js/index.js
@@ -1,43 +1,40 @@
-// object literals
+// this keyword
 
 let user = {
-    //key: value,
     name: "Sidharth Jain",
     email: "abc@gm.com",
     age: 27,
     isLoggedIn: false,
 
-    // login: function(){
-    //     // console.log("Login!");
-    //     return "Login";
-    // },
-    //with parameters
-    // login: function(nickName){
-    //     // console.log("Login!");
-    //     return `Login ${nickName}`;
-    // },
-    // logout: function(){
-    //     // console.log("Logout!");
-    //     return "Logout";
-    // }
+    blogs: [
+        {title:"Blog 1", numComment:5},
+        {title: "Blog 2", numComments:3},
+    ],
+    allBlogConsole(){
+        this.blogs.forEach((blog) => {console.log(blog)});
+        this.blogs.forEach(blog => console.log(blog.title));
+    },
     login(){
-        // console.log("Login!");
-        return `Login`;
+        // return `Login ${this}`; //Login [object Object]
+        // return `Login ${this.email}`; //Login abc@gm.com
+        this.isLoggedIn = true;
+        return `Logged in - ${this.isLoggedIn}`;
     },
     logout(){
         // console.log("Logout!");
-        return "Logout";
+        // return `Logout`;
+        this.isLoggedIn = false;
+        return `Logged out - ${this.isLoggedIn}`;
     }
 };
 
-// methods
+console.log(user.login()); 
+console.log(this);
+console.log(user.logout());
 
-console.log(user);
-console.log(user.email);
+let users = [];
 
-// if sued console.log
-// user.login(); // calling function - knows as method in this way- attached with suer object
-
-// if using return
-console.log(user.login("Sidharth"));
-
+console.log(user.blogs);
+console.log(user.blogs[0]);
+console.log(user.blogs[0].title);
+user.allBlogConsole();

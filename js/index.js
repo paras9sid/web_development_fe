ebc90907777d11fd6c52e15e35d958f7f6eb1364
Sidharth Jain <sidharthjainp9@gmail.com>
2024-11-21 20:24:18 +0530

    JS-Primitive & Reference Types

--- js/index.js
+++ js/index.js
@@ -1,18 +1,93 @@
-// Math object
+// primitive vs reference types
 
-console.log(Math);
-console.log(Math.PI);
+let numOne = 15;
+let numTwo = numOne;
+numOne = 30;
 
-console.log(Math.E);
+console.log(numOne);
+console.log(numTwo);
 
-const num = 15.3; // 
-console.log(Math.round(num)); // round - 15.3 = 15 -> till 15.49 = 15 , 15.5 or > == 16
-console.log(Math.random()); // random nmber on avery load
 
-console.log(Math.ceil(num)); // cieling 16 above value
-console.log(Math.floor(num)); // floor value self or less = 15
+// ref 
 
-console.log(Math.pow(2,5)); // 2**5 2 to the power 5
+let objOne = {
+    title: "Learning",
+    rank:1
+}
+let objTwo = objOne;
 
-console.log(Math.random()*10); // random nmber on avery load
-console.log(Math.round(Math.random()*10)); // random nmber on avery load
+console.log(objOne);
+console.log(objOne);
+console.log("");
+
+objOne.rank=5;
+
+console.log(objOne);
+console.log(objOne);
+console.log("");
+
+objTwo.rank=10;
+console.log(objOne);
+console.log(objOne);
+console.log("");
+
+let objThree = objTwo;
+objOne.rank = 15;
+
+console.log(objOne);
+console.log(objOne);
+console.log(objThree);
+console.log("");
+
+// deep copy - primitive type - will create a new copy
+
+// shallow copy = reference copy - points to same object
+
+// arrays
+let arrOne = ["sid","new"];
+let arrTwo = arrOne;
+
+// spread operator = ... -> 3 dots 
+let arrThree = [...arrOne];
+
+// ... spread operator + new item added
+let arrFour = [...arrOne, "hello","javascript","python"];
+
+// merging array
+let arrTemp = ["newvlaues","oldvalues"];
+let arrFive= [...arrOne, ...arrTemp];
+
+
+arrOne.push("copy");
+
+console.log(arrOne);
+console.log(arrTwo);
+console.log(arrThree); // only 2 elements now, new one not dependent on refernce- created new array
+console.log(arrFour);
+console.log(arrFive);
+console.log("");
+
+// obj eg.
+
+let objFour = {
+    title: "Learning",
+    rank:20
+}
+// ... operator- spread
+let objFive = {...objFour};
+
+// ... spread operator + new item added
+let objSix = {...objFour, publish:"21.11.24"}
+
+//overwriting values - adding old values with old value rank updated with new one
+let objSeven = {...objFour, rank:100};
+
+// value changed
+objFour.rank=90;
+
+console.log(objFour); //rank 90
+console.log(objOne); // rank 15 only earlie
+console.log(objSix);
+console.log(objSeven);
+console.log("");
+

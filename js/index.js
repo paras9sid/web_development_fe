b2f5dc18dd8fbf868b65bc03b0e9a37478eb8f22
Sidharth Jain <sidharthjainp9@gmail.com>
2024-11-24 12:47:02 +0530

    JS-Event Delegation/ Bubbling-stop propogation

--- js/index.js
+++ js/index.js
@@ -1,39 +1,31 @@
-// // Events-1
-
-// const productAdd = document.querySelector("button");
-// productAdd.addEventListener("click", () => {
-//     // console.log("Button clicked"); // will print this in console
-//     // productAdd.setAttribute("style", "display:none"); // button will disappear
-
-// });
-
-const product = document.querySelectorAll("li");
-product.forEach( product => {
-    // console.log(product); // check
-    product.addEventListener("click", (e) => {
-        // console.log(e); // all properties of events we can use
-        // console.log(e.target); // using and testing target property
-        // console.log(e.target.textContent);
-        e.target.remove();// removing particular nodes
-    });
-});
+// Event bubbling / delegation
 
 const addProduct = document.querySelector("ul");
 const button = document.querySelector("button");
-button.addEventListener("click", (e) => {
-    // console.log(addProduct.innerHTML); // old li detais
-    // addProduct.innerHTML += "<li>new produt added in the end by default </li>"; // added new += method
-    // console.log(addProduct.innerHTML); // new ul list with new li above added
 
-    // //addon top first not end
-    // addProduct.innerHTML = "<li>new on top</li>" + addProduct.innerHTML; // only = not += X
-    // console.log(addProduct.innerHTML);
-
-    // append - prepend method
-    // append - add at the end
-    const newLi = document.createElement("li");
-    newLi.innerText = "New Product";
-    addProduct.append(newLi);
-    console.log(addProduct.innerHTML);
+button.addEventListener("click", ()=>{
+    const li = document.createElement("li");
+    li.textContent = "New Product";
+    addProduct.append(li);
 });
 
+addProduct.addEventListener("click", (e) => {
+    // console.log("Inside ul");
+    // console.log(e.target);
+
+    // condition--
+    // Event Delegation - attaching to parent and parent is handling
+    if(e.target.tagName == "LI"){
+        // e.target.remove();
+        console.log("UL called");
+    }
+});
+
+// event bubble - as multiple events called from one click on that particular element-
+    // stop propogation method
+
+const newLi = document.querySelector(".active");
+newLi.addEventListener("click", (e)=>{
+    e.stopPropagation(); // will stop ULcalled above event - will call own current event
+    console.log("LI called");
+});

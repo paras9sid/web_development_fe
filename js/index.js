17419775d221a38c32b67e26d4970e92585badea
Sidharth Jain <sidharthjainp9@gmail.com>
2024-11-24 12:14:34 +0530

    JS-Events intro

--- js/index.js
+++ js/index.js
@@ -1,27 +1,19 @@
-// // Relationships - parent/child/sibling
+// // Events-1
 
-// const topNav = document.querySelector(".top-nav");
-// // console.log(topNav);
-// // console.log(topNav.children);
+// const productAdd = document.querySelector("button");
+// productAdd.addEventListener("click", () => {
+//     // console.log("Button clicked"); // will print this in console
+//     // productAdd.setAttribute("style", "display:none"); // button will disappear
 
-// // topNav.children.forEach(element => {
-// //     console.log(child);
-// // });
-// // error from above - index.js:7 Uncaught TypeError: topNav.children.forEach is not a functio
-
-
-// // array method to add for working
-// const topNavChildren = Array.from(topNav.children);
-// topNavChildren.forEach(element => {
-//     console.log(element);
 // });
 
-const brandName = document.querySelector("#website-name");
-console.log(brandName.parentElement); //footer
-console.log(brandName.parentElement.parentElement); // body
-console.log(brandName.parentElement.children); // htmlcollection(2)
+const product = document.querySelectorAll("li");
+product.forEach( product => {
+    // console.log(product); // check
+    product.addEventListener("click", (e) => {
+        // console.log(e); // all properties of events we can use
+        // console.log(e.target); // using and testing target property
+        console.log(e.target.textContent);
 
-// sibling
-console.log(brandName.nextElementSibling); // ul.footer-nav
-console.log(brandName.nextElementSibling.nextSibling); // #text
-console.log(brandName.nextElementSibling.nextSibling.nextSibling); // null
+    });
+});

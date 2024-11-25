05bd0b864c8e307041abd3dd771adab467bdf84a
Sidharth Jain <sidharthjainp9@gmail.com>
2024-11-25 16:30:47 +0530

    JS-form-submit event

--- js/index.js
+++ js/index.js
@@ -1,34 +1,9 @@
-// more Dom events
+// form and form events
 
-// const products = document.querySelector("ul");
-// // products.addEventListener("click", (e) => {
-// products.addEventListener("copy", (e) => {
-//     // console.log(e);
-//     if(e.target.tagName == "LI"){
-//         alert(e.target.textContent);
-//     }
-// });
-
-
-// const products = document.querySelector("ul");
-
-// // storing callback functionn  in const
-
-// const copyAlert = (e) => {
-//     if(e.target.tagName == "LI"){
-//         alert(e.target.textContent);
-//     }
-// };
-// products.addEventListener("copy", copyAlert);
-
-const webPage = document.querySelector("body");
-// webPage.addEventListener("mousemove", (e) => {
-//     // console.log(e);
-//     console.log(e.clientX, e.clientY);
-// });
-
-// scroll mouse
-webPage.addEventListener("wheel", (e) => {
+const form = document.querySelector(".feedback-form");
+form.addEventListener("submit", (e) => {
+    e.preventDefault(); // prevent submitting and refreshing of form upon clicking submit
     // console.log(e);
-    console.log(e.clientX, e.clientY);
+    console.log(form.username.value); // value of username input box
+    console.log(form.feedback.value); // value we enter in textrea after submit will show
 });

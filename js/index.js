a7afa5cd8f1db0caebe59270b03fa8e0bb812c85
Sidharth Jain <sidharthjainp9@gmail.com>
2024-11-26 14:44:59 +0530

    JS-form validation, keyboard events

--- js/index.js
+++ js/index.js
@@ -1,6 +1,7 @@
 // form and form events
 
 const form = document.querySelector(".feedback-form");
+const p = document.querySelector("p");
 
 // regular expression - regEx
 const usernamePattern =/^[a-z0-9]{6,10}$/ ;
@@ -16,13 +17,38 @@ form.addEventListener("submit", (e) => {
     const feedbackResult = feedbackPattern.test(form.feedback.value);
 
     if(userResult && feedbackResult){
-        console.log("Worked! :)")
+        p.setAttribute("class", "success"); 
+        p.textContent = "Feedback Submitted!, ty! :)"
     }else{
-        console.log("Not Worked :(")
+        p.setAttribute("class", "error");
+        p.textContent = "Pl check uname and feedback again.. :("
     }
 
     console.log(userResult, feedbackResult);    
 });
 
+form.username.addEventListener("keyup", (e) => { // keyup - user releases the key
+    // console.log(e);
+    // console.log(e.target.value);
+    const userResult = usernamePattern.test(e.target.value);
+    if(userResult){
+        form.username.setAttribute("class", "accepted");
+    }else{
+        form.username.setAttribute("class", "rejected");
+    }
+    // console.log(userResult);
 
+});
+
+
+form.feedback.addEventListener("keyup", (e) => { // keyup - user releases the key
+    const feedbackResult = feedbackPattern.test(e.target.value);
+    if(feedbackResult){
+        form.feedback.setAttribute("class", "accepted");
+    }else{
+        form.feedback.setAttribute("class", "rejected");
+    }
+    // console.log(feedbackResult);
+
+});
 

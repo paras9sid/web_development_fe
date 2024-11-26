180f1a3dbbe949427c11854ad638f53fc6dcb67b
Sidharth Jain <sidharthjainp9@gmail.com>
2024-11-26 15:25:29 +0530

    JS-HTML Events

---
+++ js/script.js
@@ -0,0 +1,35 @@
+// html events
+
+// regular expression - regEx
+const usernamePattern =/^[a-z0-9]{6,10}$/ ;
+const feedbackPattern = /^.{20,50}$/;
+
+function displayData(event){
+    event.preventDefault();
+    console.log(event);
+}
+
+function usernameValidation(event){
+    const userResult = usernamePattern.test(event.target.value);
+    if(userResult){
+        event.target.setAttribute("class", "accepted");
+    }else{
+        event.target.setAttribute("class", "rejected");
+    }
+    // console.log(event.target.value);
+}
+
+
+function feedbackValidation(event){
+    const feedbackResult = feedbackPattern.test(event.target.value);
+    if(feedbackResult){
+        event.target.setAttribute("class", "accepted");
+    }else{
+        event.target.setAttribute("class", "rejected");
+    }
+    // console.log(event.target.value);
+}
+
+function changeText(event){
+    event.target.innerText= "New Value";
+}

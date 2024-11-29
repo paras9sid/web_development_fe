ccfa3c0c9028d6c184b4174cea815f0c592e8650
Sidharth Jain <sidharthjainp9@gmail.com>
2024-11-29 18:43:16 +0530

    JS-array-map method

--- js/script.js
+++ js/script.js
@@ -1,46 +1,37 @@
-// array methods
+// array - map method
 
-const ratings = [5, 10, 9, 7, 2, 3, 8];
+const numbers = [10, 20, 30, 40, 50];
 
-// const filteredResult = ratings.filter( (rate) => {
-//     // let result = rate > 6;
-//     // return result;
+// const mappedNum = numbers.map( number => number * 5 );
 
-//     return rate > 6;
-// });
-
-//1 liner of above.
-const filteredResult = ratings.filter(rate =>rate > 6 );
-
-// console.log(filteredResult);
-// console.log(ratings);
-
-const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-// const even = numbers.filter(num=>{
-//     let result = num % 2 == 0;
-//     return result;
-// });
-
-// const odd = numbers.filter(num=>{
-//     let result = num % 2 == 0;
-//     return !result;
-// });
-
-//1 liner
-const even = numbers.filter( num=> num % 2 == 0 );
-const odd = numbers.filter( num => num % 2 != 0 );
-// console.log(even, odd);
-
-// objects
-const movieList = [
-    {name: "Coffe Day", rating:9},
-    {name: "AbD", rating:5},
-    {name: "ZXCV", rating:6}
-];
-
-// console.log(movieList[0]);
-const filteredMovie = movieList.filter( (movie, index) => {
-    return  movie.rating > 5;
+const mappedNum = numbers.map( number => {
+    if(number > 30){
+        return number + 5;
+    }else{
+        return number;
+    }
 });
 
-console.log(filteredMovie);
+// console.log(mappedNum);
+
+const products = [
+    {name: "Breed", price:25, tax:0.05},
+    {name: "Breed", price:65, tax:0.05},
+    {name: "Breed", price:15, tax:0},
+    {name: "Breed", price:125, tax:0.18},
+];
+
+const mappedProducts = products.map((product)=>{
+    // return product.price * product.tax;
+    console.log(product.price + product.price * product.tax);
+
+    // new price updated consition
+    const newPrice = product.price + product.price * product.tax;
+    //returning object itself
+    // old price
+    // return {name: product.name, price: product.price, tax: product.tax};
+
+    // new price updated with tax
+    return {name: product.name, price: newPrice, tax: `tax applied : ${product.tax}`};
+});
+console.log(mappedProducts);

03be3f063630836b3108bea4ffa57b6c442dd103
Sidharth Jain <sidharthjainp9@gmail.com>
2024-11-29 18:13:30 +0530

    JS-ARray methods=filter met.

--- js/script.js
+++ js/script.js
@@ -1,35 +1,46 @@
-// html events
+// array methods
 
-// regular expression - regEx
-const usernamePattern =/^[a-z0-9]{6,10}$/ ;
-const feedbackPattern = /^.{20,50}$/;
+const ratings = [5, 10, 9, 7, 2, 3, 8];
 
-function displayData(event){
-    event.preventDefault();
-    console.log(event);
-}
+// const filteredResult = ratings.filter( (rate) => {
+//     // let result = rate > 6;
+//     // return result;
 
-function usernameValidation(event){
-    const userResult = usernamePattern.test(event.target.value);
-    if(userResult){
-        event.target.setAttribute("class", "accepted");
-    }else{
-        event.target.setAttribute("class", "rejected");
-    }
-    // console.log(event.target.value);
-}
+//     return rate > 6;
+// });
 
+//1 liner of above.
+const filteredResult = ratings.filter(rate =>rate > 6 );
 
-function feedbackValidation(event){
-    const feedbackResult = feedbackPattern.test(event.target.value);
-    if(feedbackResult){
-        event.target.setAttribute("class", "accepted");
-    }else{
-        event.target.setAttribute("class", "rejected");
-    }
-    // console.log(event.target.value);
-}
+// console.log(filteredResult);
+// console.log(ratings);
 
-function changeText(event){
-    event.target.innerText= "New Value";
-}
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+// const even = numbers.filter(num=>{
+//     let result = num % 2 == 0;
+//     return result;
+// });
+
+// const odd = numbers.filter(num=>{
+//     let result = num % 2 == 0;
+//     return !result;
+// });
+
+//1 liner
+const even = numbers.filter( num=> num % 2 == 0 );
+const odd = numbers.filter( num => num % 2 != 0 );
+// console.log(even, odd);
+
+// objects
+const movieList = [
+    {name: "Coffe Day", rating:9},
+    {name: "AbD", rating:5},
+    {name: "ZXCV", rating:6}
+];
+
+// console.log(movieList[0]);
+const filteredMovie = movieList.filter( (movie, index) => {
+    return  movie.rating > 5;
+});
+
+console.log(filteredMovie);
